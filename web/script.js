e9b4e11bd25332cf452e8f28855c2126962e1b8f
sugitou <sugi_tou108@yahoo.co.jp>
2021-09-26 12:17:31 +0900

    修正

--- web/script.js
+++ web/script.js
@@ -2,16 +2,19 @@
 async function rakuten_func(kw_search, csv_name, box_name, select_api) {
     let val = await eel.rakuten_system(kw_search, csv_name, box_name, select_api)();
     if (select_api == "market_search") {
+        document.getElementById('item_name').value = ""
         for (let i = 0; i < val.length; i++) {
             let num = i + 1
             document.getElementById('item_name').value += "【" + num + "件目】" + val[i] + "\n";
         }
     }else if (select_api == "books_search") {
+        document.getElementById('book_name').value = ""
         for (let i = 0; i < val.length; i++) {
             let num = i + 1
             document.getElementById('book_name').value += "【" + num + "件目】" + val[i] + "\n";
         }
     }else {
+        document.getElementById('hotel_name').value = ""
         for (let i = 0; i < val.length; i++) {
             let num = i + 1
             document.getElementById('hotel_name').value += "【" + num + "件目】" + val[i] + "\n";
@@ -30,7 +33,6 @@ const wrap03 = document.getElementById("wrap03");
 // 切り替え関数
 function vis_invis_01(){
     if(wrap01.style.display=="none"){
-        // noneで非表示
         wrap01.style.display ="block";
         wrap02.style.display ="none";
         wrap03.style.display ="none";
@@ -42,7 +44,6 @@ function vis_invis_01(){
 }
 function vis_invis_02(){
     if(wrap02.style.display=="none"){
-        // noneで非表示
         wrap01.style.display ="none";
         wrap02.style.display ="block";
         wrap03.style.display ="none";
@@ -54,7 +55,6 @@ function vis_invis_02(){
 }
 function vis_invis_03(){
     if(wrap03.style.display=="none"){
-        // noneで非表示
         wrap01.style.display ="none";
         wrap02.style.display ="none";
         wrap03.style.display ="block";
